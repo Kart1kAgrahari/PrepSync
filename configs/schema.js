b207372a0c81999c879bb379b32d59ee571bb3df
Kart1kAgrahari <kartikagrahari.03@gmail.com>
2025-05-14 01:56:13 +0530

    0.5

--- configs/schema.js
+++ configs/schema.js
@@ -42,3 +42,26 @@ export  const  PAYMENT_RECORD_TABLE = pgTable('paymentRecord',{
     customerId:varchar(),
     sessionId:varchar(),
 })
+
+export const MockInterview=pgTable('mockInterview',{
+    id:serial('id').primaryKey(),
+    jsonMockResp:text('jsonMockResp').notNull(),
+    jobPosition:varchar('jobPosition').notNull(),
+    jobDesc:varchar('jobDesc').notNull(),
+    jobExperience:varchar('jobExperience').notNull(),
+    createdBy:varchar('createdBy').notNull(),
+    createdAt:varchar('createdAt'),
+    mockId:varchar('mockId').notNull()
+})
+
+export const UserAnswer=pgTable('userAnswer',{
+    id:serial('id').primaryKey(),
+    mockIdRef:varchar('mockId').notNull(),
+    question:varchar('question').notNull(),
+    correctAns:text('correctAns'),
+    userAns:text('userAns'),
+    feedback:text('feedback'),
+    rating:varchar('rating'),
+    userEmail:varchar('userEmail'),
+    createdAt:varchar('createdAt'),
+})
