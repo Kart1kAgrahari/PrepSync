47353808c8ce1d0cb3c2e0da4ac9de5d5789f0ee
Kart1kAgrahari <kartikagrahari.03@gmail.com>
2025-05-14 11:46:29 +0530

    14/05/25

--- app/dashboard/_components/SideBar.jsx
+++ app/dashboard/_components/SideBar.jsx
@@ -47,29 +47,7 @@ function Sidebar() {
         <Button > + Create New</Button>
         </Link>
         <div className="mt-5">
-          {/* {MenuList.map((menu, index) => (
-            <div
-              key={index}
-              className={`flex gap-5 items-center p-3 
-                hover:bg-slate-200 rounded-lg cursor-pointer mt-3
-                ${path == menu.path && "bg-slate-200"}`}
-            >
-              <menu.icon />
-              <h2>{menu.name}</h2>
-            </div>
-          ))} */}
-          {/* {MenuList.map((menu, index) => (
-  <Link href={menu.path} key={index}>
-    <div
-      className={`flex gap-5 items-center p-3 
-        hover:bg-slate-200 rounded-lg cursor-pointer mt-3
-        ${path === menu.path ? "bg-slate-200" : ""}`}
-    >
-      <menu.icon />
-      <h2>{menu.name}</h2>
-    </div>
-  </Link>
-))} */}
+         
 {MenuList.map((menu, index) =>
             menu.action ? (
               <div
